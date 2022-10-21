TenX-OS/packages_apps_Settings

Settings: Integrate Our Ten-X into Settings
  * Add it on dashboard

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
Change-Id: Icc09eb0b7a2da824705c8c7932ae6acb8c2059ed

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -160,6 +160,8 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.tenx.settings.TenXSettings;
+
 public class SettingsGateway {
 
     /**
@@ -307,7 +309,9 @@ public class SettingsGateway {
             GestureNavigationSettingsFragment.class.getName(),
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
-            MediaControlsSettings.class.getName()
+            MediaControlsSettings.class.getName(),
+            // Ten-X Additions
+            TenXSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -347,5 +351,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // Ten-X Additions
+            Settings.TenXSettingsActivity.class.getName(),
     };
 }

tenx-tmp/packages_apps_Settings-1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -179,6 +179,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.tenx.settings.TenXSettings;
+
 public class SettingsGateway {
 
     /**
@@ -345,7 +347,10 @@ public class SettingsGateway {
             MobileNetworkSettings.class.getName(),
             AppLocaleDetails.class.getName(),
             TurnScreenOnSettings.class.getName(),
-            TurnScreenOnDetails.class.getName()
+            TurnScreenOnDetails.class.getName(),
+
+            // Ten-X Additions
+            TenXSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -388,5 +393,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // Ten-X Additions
+            Settings.TenXSettingsActivity.class.getName(),
     };
 }