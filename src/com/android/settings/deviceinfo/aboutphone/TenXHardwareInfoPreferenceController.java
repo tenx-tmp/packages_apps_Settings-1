19e37e8e935fdded78776906bd098eecdf543fa6
elluzion <dyrex2004@gmail.com>
2022-10-28 07:22:10 +0000

    Settings: Get Storage & Resolution from the System
    Co-authored-by: Androbuddy <anirudhchimpidi8@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: Ib9898eadfa4bb67abb003ee3d93826ae65f317a9

--- src/com/android/settings/deviceinfo/aboutphone/TenXHardwareInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/aboutphone/TenXHardwareInfoPreferenceController.java
@@ -17,6 +17,12 @@
 package com.android.settings.deviceinfo.aboutphone;
 
 import android.content.Context;
+import android.os.Environment;
+import android.os.StatFs;
+import android.view.Display;
+import android.view.WindowManager;
+import android.util.DisplayMetrics;
+import android.graphics.Point;
 import android.widget.TextView;
 
 import androidx.preference.PreferenceScreen;
@@ -25,17 +31,85 @@ import com.android.settings.R;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.widget.LayoutPreference;
 
+import com.android.internal.util.MemInfoReader;
+
+import java.io.File;
+
 public class TenXHardwareInfoPreferenceController extends AbstractPreferenceController {
 
     private static final String KEY_TENX_HARDWARE_INFO = "tenx_hardware_info";
+    static String aproxStorage;
 
     public TenXHardwareInfoPreferenceController(Context context) {
         super(context);
     }
 
+    public static String getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSizeLong();
+        long totalBlocks = stat.getBlockCountLong();
+        double total = (totalBlocks * blockSize)/ 1073741824;
+        int lastval = (int) Math.round(total);
+            if ( lastval > 0  && lastval <= 16){
+                aproxStorage = "16";
+            } else if (lastval > 16 && lastval <=32) {
+                aproxStorage = "32";
+            } else if (lastval > 32 && lastval <=64) {
+                aproxStorage = "64";
+            } else if (lastval > 64 && lastval <=128) {
+                aproxStorage = "128";
+            } else if (lastval > 128 && lastval <= 256) {
+                aproxStorage = "256";
+            } else if (lastval > 256 && lastval <= 512) {
+                aproxStorage = "512";
+            } else if (lastval > 512) {
+                aproxStorage = "512+";
+            } else aproxStorage = "null";
+        return aproxStorage;
+    }
+
+    public static int getTotalRAM() {
+        MemInfoReader memReader = new MemInfoReader();
+        memReader.readMemInfo();
+        String aproxStorage;
+        double totalmem = memReader.getTotalSize();
+        double gb = (totalmem / 1073741824) + 0.1f; // Cause 4gig devices show memory as 3.48 .-.
+        int gigs = (int) Math.round(gb);
+        return gigs;
+    }
+
+    public static String getScreenRes(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y + getNavigationBarHeight(windowManager);
+        return width + "x" + height;
+    }
+
+    private static int getNavigationBarHeight(WindowManager wm) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(metrics);
+        int usableHeight = metrics.heightPixels;
+        wm.getDefaultDisplay().getRealMetrics(metrics);
+        int realHeight = metrics.heightPixels;
+        if (realHeight > usableHeight)
+            return realHeight - usableHeight;
+        else
+            return 0;
+    }
+
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
+        final LayoutPreference TenXHardwareInfoPreference = screen.findPreference(KEY_TENX_HARDWARE_INFO);
+        final TextView storage = (TextView) TenXHardwareInfoPreference.findViewById(R.id.storage_summary);
+        final TextView resolution = (TextView) TenXHardwareInfoPreference.findViewById(R.id.resolution_summary);
+
+        storage.setText(String.valueOf(getTotalInternalMemorySize()) + "GB + " + String.valueOf(getTotalRAM()) + "GB");
+        resolution.setText(getScreenRes(mContext));
     }
 
     @Override
